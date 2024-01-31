9814db4dcf53edd2df980d3d3a05e8d02adecaa9
Daniel Rivas <drivas8891@gmail.com>
2024-01-30 16:47:23 -0800

    Created a method to build a binary tree based using a vector of integers. Null values are represented by INT_MIN due to limitations in C++ versions prior to version 17.

--- Source.cpp
+++ Source.cpp
@@ -5,23 +5,17 @@
 using namespace std;
 
 vector<double> averageOfLevels(TreeNode* root);
+TreeNode* buildTree(const vector<int>& vec);
 void deleteTree(TreeNode* root);
 void printVector(const vector<double>& container);
 
 int main() {
-	// Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. 
-	// Answers within 10-5 of the actual answer will be accepted.
+	// Given the root of a binary tree, return the average value of the nodes on each level in the form of an array.
 	
 	TreeNode* emptyTree = nullptr;
 	printVector(averageOfLevels(emptyTree)); // Expected: EMPTY
 
-	// Manually build a tree for testing purposes
-	TreeNode* filledTree = new TreeNode(3);
-	filledTree->left = new TreeNode(9);
-	filledTree->right = new TreeNode(20);
-	filledTree->right->left = new TreeNode(15);
-	filledTree->right->right = new TreeNode(7);
-
+	TreeNode* filledTree = buildTree({ 3, 9, 20, INT_MIN, INT_MIN, 15, 7 });
 	printVector(averageOfLevels(filledTree)); // Expected: [3, 14.5, 11]
 	deleteTree(filledTree);
 
@@ -65,6 +59,33 @@ vector<double> averageOfLevels(TreeNode* root) {
 		averages.push_back(levelSum / queueSize);
 	}
 	return averages;
+}
+
+TreeNode* buildTree(const vector<int>& vec) {
+	if (vec.empty() || vec[0] == INT_MIN) return nullptr;
+
+	TreeNode* root = new TreeNode(vec[0]);
+	queue<TreeNode*> q;
+	q.push(root);
+
+	for (size_t i = 1; i < vec.size(); i++) {
+		TreeNode* parent = q.front();
+		if (i % 2 == 1) { // Odd index, potential left child
+			if (vec[i] != INT_MIN) {
+				parent->left = new TreeNode(vec[i]);
+				q.push(parent->left);
+			}
+		}
+		else { // Even index, potential right child
+			if (vec[i] != INT_MIN) {
+				parent->right = new TreeNode(vec[i]);
+				q.push(parent->right);
+			}
+			q.pop(); // Finished with this parent
+		}
+	}
+
+	return root;
 }
 
 void deleteTree(TreeNode* root) {
